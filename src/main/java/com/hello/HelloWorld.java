ef29d65464e485cce819355890eb94548b411f6a
Eric Mastrangelo <ericromanmastrangelo@gmail.com>
2018-01-23 13:45:33 -0300

    Adding Spring Boot

--- src/main/java/com/hello/HelloWorld.java
+++ src/main/java/com/hello/HelloWorld.java
@@ -1,8 +1,14 @@
-package main.java.com.hello;
+package com.hello;
+
+import org.joda.time.LocalTime;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+    	
+		LocalTime currentTime = new LocalTime();
+		System.out.println("The current local time is: " + currentTime);
+
+		Greeter greeter = new Greeter();
+		System.out.println(greeter.sayHello());
     }
 }
