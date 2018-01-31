1f9caa225c9da9cd4ccd8f4a04ee7bb6b289aadb
Eric Mastrangelo <ericromanmastrangelo@gmail.com>
2018-01-31 16:41:27 -0300

    AlgorithmLingPipe Integrated

--- src/main/java/algorithms/AlgorithmLingPipe.java
+++ src/main/java/algorithms/AlgorithmLingPipe.java
@@ -1,6 +1,5 @@
 package algorithms;
 
-
 import java.io.File;
 import java.io.IOException;
 
@@ -8,36 +7,34 @@ import com.aliasi.classify.ConditionalClassification;
 import com.aliasi.classify.LMClassifier;
 import com.aliasi.util.AbstractExternalizable;
 
-public class AlgorithmLingPipe implements AlgoritmosClasificacion {//clasifyerLoader
+public class AlgorithmLingPipe implements AlgoritmosClasificacion {// clasifyerLoader
 	String[] categories;
 	LMClassifier clasificador;
 
 	public AlgorithmLingPipe() {
-	
-	try {
-		clasificador= (LMClassifier) AbstractExternalizable.readObject(new File("classifier.txt"));
-		categories = clasificador.categories();
-	}
-	catch (ClassNotFoundException e) {
-		e.printStackTrace();
-	}
-	catch (IOException e) {
-		e.printStackTrace();
-	}
+
+		try {
+			clasificador = (LMClassifier) AbstractExternalizable.readObject(new File("classifier.txt"));
+			categories = clasificador.categories();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@Override
 	public int clasificar(String tweet) {
 		ConditionalClassification classification = clasificador.classify(tweet);
-		System.out.println(clasificador.classify(tweet));
+		// System.out.println(clasificador.classify(tweet));
 
-		if (classification.bestCategory().equals("pos")){
+		if (classification.bestCategory().equals("pos")) {
 			return 3;
-			
-		}else{
+
+		} else {
 			return 1;
 		}
 	}
-	
+
 }
